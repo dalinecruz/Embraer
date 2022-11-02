887ba39a4f1eb0775c9e47ffabd1b96fd761e90d
Daline Cruz <assinaturas.dalineecruz@gmail.com>
2022-11-02 16:59:38 -0300

    conclusão da atividade 2

---
+++ Modulo1/desafio/atividade2.py
@@ -0,0 +1,9 @@
+# -*- coding: iso-8859-1 -*-
+'''
+Created on 24 de out. de 2022
+
+@author: daline
+'''
+# reescrevendo a atividade 1 com o conceito de compreensão de dicionários
+qtd_letras = {item:len(item)for item in ['Maria', 'Julieta', 'Fernando', 'Cristiano', 'Julieta', 'Maria', 'Fernando', 'Cláudio']}
+print(qtd_letras)
