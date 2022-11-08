9a0e3f32b9c4322bddd18ac2c8389a8b51c009cd
Daline Cruz <assinaturas.dalineecruz@gmail.com>
2022-11-07 21:21:48 -0300

    Conclusão do código 5

---
+++ Modulo2/trabalho_pratico/ex_cod_5.py
@@ -0,0 +1,14 @@
+# -*- coding: iso-8859-1 -*-
+'''
+Created on 7 de nov. de 2022
+
+@author: daline
+'''
+# importação das bibliotecas
+import numpy as np
+import pandas as pd
+
+## Código 5 ##
+A = np.ones((2,2))
+X = 2*A
+print("X:\n", X)
