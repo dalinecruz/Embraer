4100f43aa803d4b1222847664133ecfab598f63f
Daline Cruz <assinaturas.dalineecruz@gmail.com>
2022-11-27 19:12:55 -0300

    Criação de formulários

--- HTMLCSSModulo3/flask/env/app.py
+++ HTMLCSSModulo3/flask/env/app.py
@@ -1,13 +1,20 @@
 import os
 from flask import Flask, render_template
+from form import NameForm
 
 template_dir = os.path.abspath('./templates')
 
 app = Flask(__name__, template_folder=template_dir)
 
-@app.route('/user')
+app.config['SECRET_KEY'] = 'NSLKNDOCNCONCSDLCNFCADJSNCCSND'
 
-@app.route('/user/<name>')
+@app.route('/', methods=['GET', 'POST'])
 def index(name=None):
-    print(name)
-    return render_template('index.html', name=name)
+    
+    name = None
+    form = NameForm()
+    if form.validate_on_submit():
+        name = form.name.data
+        form.name.data = ""
+    
+    return render_template('index.html', name=name, form=form)
